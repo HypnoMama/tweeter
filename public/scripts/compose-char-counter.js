7a2c5429593fa75573cb5d2038435d640f20cf6b
HypnoMama <sarahhermsen@gmail.com>
2018-07-17 21:35:25 +0000

    refactored code for efficiency by declaring variables outside of the on input function

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,28 +1,16 @@
 $(document).ready(function() {
-  // let count = 140;
-  let counterBlack = $('.counter');
-  let counterRed = $('.counterRed')
-  $('textarea').on('input', function() {
+  const counter = $('.counter')[0];
+  const counterBlack = $('.counter');
+  const counterRed = $('.counterRed')
+  const textarea = $('textarea')
+  $(textarea).on('input', function() {
     let keepCount = $(this).val();
-    console.log(140 - keepCount.length);
-    let parent = $(this).parent('form')[0];
-    let span = $(this).closest(parent).find('span')[0]
-    console.log(span)
-    $(span).text(140 - keepCount.length)
+    $(counter).text(140 - keepCount.length)
     if(keepCount.length >= 140){
-      $(span).toggleClass('counterRed', true);
+      $(counter).toggleClass('counterRed', true);
     } else {
-      $(span).toggleClass('counterRed', false);
+      $(counter).toggleClass('counterRed', false);
     }
-
-
-
-    // $('.counter').text(count -= 1);
-    // if (count <= 0) {
-    //   $('.counter').css('color', 'red');
-    // }
-});
-
-
+  });
 });
 
