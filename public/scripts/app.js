617eb14269b05e47f49fdc94df34d056f7f357cc
HypnoMama <sarahhermsen@gmail.com>
2018-07-18 18:55:11 +0000

    added renderTweets function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,8 @@
 
 $(document).ready(function(){
 
-  const tweets = {
+  const tweets = [
+  {
     "user": {
       "name": "Newton",
       "avatars": {
@@ -20,7 +21,38 @@ $(document).ready(function(){
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
   }
+]
+
 
   function createTweetElement(data){
     let name = data.user.name;
@@ -69,13 +101,18 @@ $(document).ready(function(){
 
   }
 
-  const $tweet = createTweetElement(tweets);
-  const $tweetsContainer = $('#tweets-container')
-  $tweetsContainer.append($tweet);
+  function renderTweets(aryOfTweetObjs) {
+    aryOfTweetObjs.forEach(function(tweet) {
+      let newTweet = createTweetElement(tweet);
+      $('#tweets-container').prepend(newTweet);
+    });
+  }
+
+  renderTweets(tweets);
 
 
 
-
+  //when we post it call loadData()
   //end
 });
 
