33d476bb82765781dc78fd592f27e4698e0e9b73
HypnoMama <sarahhermsen@gmail.com>
2018-07-18 19:00:11 +0000

    did clean up of old comments and added missing semi-colons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,37 +51,37 @@ $(document).ready(function(){
     },
     "created_at": 1461113796368
   }
-]
+  ]
 
 
   function createTweetElement(data){
     let name = data.user.name;
-    let handle = data.user.handle
-    let avatar = data.user.avatars.small
-    let content = data.content.text
-    let createdAt = data.created_at
-    let dateCreated = new Date(createdAt)
+    let handle = data.user.handle;
+    let avatar = data.user.avatars.small;
+    let content = data.content.text;
+    let createdAt = data.created_at;
+    let dateCreated = new Date(createdAt);
     let currentDate = new Date();
 
-    let $span = $('<span>').attr('id', 'handle')
-    let $image = $('<img>').attr('src', avatar)
-    let $text = $('<p>')
-    let $footer = $('<footer>')
+    let $span = $('<span>').attr('id', 'handle');
+    let $image = $('<img>').attr('src', avatar);
+    let $text = $('<p>');
+    let $footer = $('<footer>');
     let $spanFooterIcons = $('<span>').attr('id', 'icons');
-    let $flag = $('<i>').addClass('fas fa-flag')
-    let $retweet = $('<i>').addClass('fas fa-retweet')
-    let $heart = $('<i>').addClass('fas fa-heart')
+    let $flag = $('<i>').addClass('fas fa-flag');
+    let $retweet = $('<i>').addClass('fas fa-retweet');
+    let $heart = $('<i>').addClass('fas fa-heart');
     let daysAgo = moment(dateCreated).fromNow();
 
-    let $article = $('<article>').addClass('prev-tweet');//creates the article element
-    let $header = $('<header>')
+    let $article = $('<article>').addClass('prev-tweet');
+    let $header = $('<header>');
 
-    //need to add info into header before appending
-    $header.text(name)//adds the name text to the header
-    $header.append($image)//appends the image tag to the header
-    $span.text(handle);//adds the persons handle to the header span
-    $header.append($span);//appends handle span to header
-    $footer.text(daysAgo);//adds text to the footer
+
+    $header.text(name);
+    $header.append($image);
+    $span.text(handle);
+    $header.append($span);
+    $footer.text(daysAgo);
     $spanFooterIcons.append($flag).append($retweet).append($heart);
     $footer.append($spanFooterIcons)//
     $text.text(content);
@@ -94,11 +94,7 @@ $(document).ready(function(){
     $article.append($text);
     $article.append($footer);
 
-    return $article
-    //no need to return the html as we've appended everything to the
-    // article itself
-
-
+    return $article;
   }
 
   function renderTweets(aryOfTweetObjs) {
