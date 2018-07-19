52749421188e0b5445e741b70d13458ade70cb0d
HypnoMama <sarahhermsen@gmail.com>
2018-07-19 20:46:20 +0000

    modified getTweets and saveTweets functions to use the mongo db

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -7,21 +7,38 @@ const simulateDelay = require("./util/simulate-delay");
 module.exports = function makeDataHelpers(db) {
   return {
 
+    //modify to save to mongo => insertOne()
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+    //take the newTweet and add it to database
+        db.collection("tweets").insertOne(newTweet, (err, res) => {
+          console.log("New tweet added");
+        });
+
         callback(null, true);
-      });
     },
 
+    //modify to get mongo tweets => .find()
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      // console.log(db.tweets.find())
+      db.collection("tweets").find().toArray((err, tweets) => {
+        const sortNewestFirst = (a, b) => {
+          a.created_at - b.created_at;
+        }
+
+        callback(null, tweets.sort(sortNewestFirst));
       });
+
     }
+    //  getTweets: function(callback) {
+    //   db.collection("tweets").find().toArray((err, tweets) => {
+    //   if (err) {
+    //     return callback(err);
+    //   }
+    //   callback(null, tweets);
+    //   });
+    // }
 
   };
 }
